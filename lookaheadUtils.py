c82ba1126b3928e2c05216099aeea2c8e1b10b9a
Zahar <znaumets@gmail.com>
2023-10-30 14:29:22 +0200

    I have created lookaheadUtils.py file

---
+++ lookaheadUtils.py
@@ -0,0 +1,6 @@
+from grammar import Grammar, NonTerminal, BaseTerminalString
+from typing import List, Set
+
+
+def firstK(k: int, grammar: Grammar) -> Set[dict[NonTerminal, List[BaseTerminalString]]]:
+    pass
